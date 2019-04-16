3e250bb5c38d1a118e99b3fb5491cf9d5f43243e
josemak25 <amakirij@gmail.com>
2019-04-16 14:05:56 +0100

    fixed broken userClass test tes

--- src/userClass.js
+++ src/userClass.js
@@ -2,9 +2,11 @@ const memberClass = require("./memberClass");
 
 //CREATING THE USERCLASS CONSTRUCTOR
 function User(name, email, password) {
-  memberClass(this, name, email, password);
+  memberClass.call(this, name, email, password);
 }
 
 //SETTING THE USERCLASS A SUBCLASS OF MEMBERCLASS
 User.prototype = Object.create(memberClass.prototype);
 User.prototype.constructor = User;
+
+const joe = new User("meme Ken", "meme@gmail.com", "swordfish");
