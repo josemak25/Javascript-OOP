4ad5c831320907b8572a8d4b7a0afe37bc25d27e
josemak25 <amakirij@gmail.com>
2019-04-16 16:12:59 +0100

    cleaned inheritance hierarchy and made userClass superClass and adminClass subClass to super

--- src/adminClass.js
+++ src/adminClass.js
@@ -1,12 +1,18 @@
-const memberClass = require("./memberClass");
+const UserClass = require("./userClass");
 
 //CREATING THE USERCLASS CONSTRUCTOR
 function Admin(name, email, password) {
-  memberClass.call(this, name, email, password);
+  UserClass.call(this, name, email, password);
 }
 
 //SETTING THE USERCLASS A SUBCLASS OF MEMBERCLASS
-Admin.prototype = Object.create(memberClass.prototype);
+Admin.prototype = Object.create(UserClass.prototype);
 Admin.prototype.constructor = Admin;
 
 const andy = new Admin("Andy Doe", "Andy@gmail.com", "sword32fish");
+const joe = new Admin("meme Ken", "meme@mail.com", "swordfish");
+const dan = new Admin("meme Ken", "mememail.com", "swordfish");
+const dan2 = new Admin("meme Ken", "meme@mil.com", "swordfish");
+const dan3 = new UserClass("meme Ken", "meme@mail.om", "swordfish");
+const dan4 = new UserClass("meme Ken", "meme@mail.c", "swordfish");
+const dan5 = new UserClass("meme Ken", "mmail.com", "swordfish");
--- src/userClass.js
+++ src/userClass.js
@@ -1,12 +1,31 @@
-const memberClass = require("./memberClass");
+const fs = require("fs");
 
-//CREATING THE USERCLASS CONSTRUCTOR
 function User(name, email, password) {
-  memberClass.call(this, name, email, password);
+  (this.name = name), (this.email = email), (this.password = password);
+
+  const userData = this;
+
+  // ADDING EVERY USER CREATED TO DATAASE
+  try {
+    const dbResponse = fs.readFileSync("./db/dataBase.json", "utf8");
+    const dataBase = JSON.parse(dbResponse);
+    // console.log(dataBase);
+
+    // for (users of dataBase.userDATABASE) {
+    //   if (users.email === userData.email) {
+    //     return console.log("User already exits");
+    //   }
+    // }
+    dataBase.userDATABASE.push(userData);
+    console.log(dataBase);
+
+    fs.writeFileSync(
+      "./db/dataBase.json",
+      JSON.stringify(dataBase, null, 2),
+      "utf8"
+    );
+  } catch (error) {
+    return new Error("sorry could not find file path");
+  }
 }
-
-//SETTING THE USERCLASS A SUBCLASS OF MEMBERCLASS
-User.prototype = Object.create(memberClass.prototype);
-User.prototype.constructor = User;
-
-const joe = new User("meme Ken", "meme@gmail.com", "swordfish");
+module.exports = User;
